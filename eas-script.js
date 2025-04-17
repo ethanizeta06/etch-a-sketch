2007cad7687ea9bedbf140dfe9cd5357911a2d35
ethanizeta06 <ethanizetavelez06@gmail.com>
2025-04-17 21:58:20 +0200

    got the resizable grid working
    button placement and styling is missing

--- eas-script.js
+++ eas-script.js
@@ -1,6 +1,6 @@
 document.body.style.backgroundColor = "black";
 const wrapper = document.createElement("div");
-wrapper.setAttribute("style","display:flex; justify-content: center; align-items: center; height: 100vh");
+wrapper.setAttribute("style","display:flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh");
 const container = document.createElement("div");
 container.setAttribute("class","container");
 container.setAttribute("style","width: 400px; height: 400px;display: flex;flex-wrap: wrap; ");
@@ -20,3 +20,37 @@ gridNodeList.forEach (div => {
         target.style.backgroundColor = "#89CFF0";
     })
 })
+
+
+const divNodeList = document.querySelectorAll(".hovering");
+const button = document.createElement("button");
+button.setAttribute("style", "display: flex; justify-content: center; font-size: 30px; border-radius: 30%; background-color: white; margin-bottom: 20px;");
+wrapper.appendChild(container);
+wrapper.appendChild(button);
+document.body.appendChild(wrapper);
+
+
+button.addEventListener('click',(event)=>{
+    let userInput
+    do{
+     userInput = parseInt(prompt("Number of squares per side for the new grid: "));
+    }while(userInput>100 || userInput<0);
+    container.innerHTML = "";
+    let newSquareSize = 400/userInput;
+    for (let i=1; i<userInput*userInput; i++)
+    {
+        const square = document.createElement("div");
+        square.setAttribute("style",`width: ${newSquareSize}px; height: ${newSquareSize}px; background-color: white;`);
+        square.setAttribute("class","hovering");
+        container.appendChild(square);
+    }
+    // Reapply hover effect to new squares
+const newSquares = document.querySelectorAll(".hovering");
+newSquares.forEach(div => {
+  div.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = "#89CFF0";
+  });
+});
+
+})
+document.body.appendChild(button);
